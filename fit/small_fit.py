49666e4689a50f48df5b4aa0c0e5558e8826bba7
horoshenkih <horoshenkih91@gmail.com>
2016-07-07 11:34:21 +0300

    first prototype of fit function (worse than 'degrees' heuristic)

--- fit/small_fit.py
+++ fit/small_fit.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from itertools import combinations
 
 import numpy as np
+from scipy.optimize import minimize
 
 def cosh_d(v1, v2):
     r1, phi1 = v1
@@ -11,8 +12,36 @@ def cosh_d(v1, v2):
 
     return np.cosh(r1) * np.cosh(r2) - np.sinh(r1) * np.sinh(r2) * np.cos(phi1 - phi2)
 
+class Q:
+    "loss function to minimize"
+    def __init__(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
+        n = len(vertices)
+        self.coshR = np.cosh(2 * np.log(n))
+
+    def __call__(self, x):
+        "x = [r1, phi1, r2, phi2, ...] for vertex sequence v1, v2, ..."
+        value = 0.
+        assert len(x) % 2 == 0
+        for (v1, v2) in combinations(self.vertices, 2):
+            i1 = self.vertices.index(v1)
+            i2 = self.vertices.index(v2)
+            r1 = x[2*i1]
+            phi1 = x[2*i1+1]
+            r2 = x[2*i2]
+            phi2 = x[2*i2+1]
+            pred_edge = 1. if cosh_d((r1, phi1), (r2, phi2)) <= self.coshR else 0.
+            if (v1, v2) in self.edges or (v2, v1) in self.edges:
+                true_edge = 1.
+            else:
+                true_edge = 0.
+            value += (pred_edge - true_edge)**2
+        return value
+
 def find_embeddings(vertices, edges, mode):
     "find (r, phi) for each vertex"
+    vertices = list(vertices)
     n = len(vertices)
     R = 2 * np.log(n)
 
@@ -26,10 +55,23 @@ def find_embeddings(vertices, edges, mode):
         for v1, v2 in edges:
             degrees[v1] += 1
             degrees[v2] += 1
-
         return {v: (2*np.log(n / degrees[v]), np.random.uniform(0.0, 2*np.pi)) for v in vertices}
     elif mode == 'fit':
-        raise Exception('not implemented yet')
+        x0 = []
+        for (r, phi) in zip([np.random.uniform(0.0, R) for v in vertices], [np.random.uniform(0.0, 2*np.pi) for v in vertices]):
+            x0.append(r)
+            x0.append(phi)
+        x0 = np.array(x0)
+
+        q = Q(vertices, edges)
+        res = minimize(q, x0)
+        retval = {}
+        for i in range(len(vertices)):
+            r = res.x[2*i]
+            phi = res.x[2*i+1]
+            retval[vertices[i]] = (r, phi)
+
+        return retval
     else:
         raise Exception('unknown mode')
 
