5e8dbafc47d7cd866cf0bbcc3d07ff12bef5aa9d
horoshenkih <horoshenkih91@gmail.com>
2016-10-15 16:40:18 +0300

    do not evaluate contingency matrix

--- eval-embedding.py
+++ eval-embedding.py
@@ -3,6 +3,7 @@ import argparse
 from collections import defaultdict
 
 import numpy as np
+import networkx as nx
 from sklearn.neighbors import BallTree
 
 from lib.graph import read_graph_from_file, read_embeddings_from_file, distance, make_edge
@@ -11,34 +12,36 @@ def evaluate_embeddings(embeddings, edges):
     "evaluate quality of embeddings compared with real elge set"
     report = []
 
-    n = len(embeddings.keys())
-    R = 2 * np.log(n)
-    coshR = np.cosh(R)
-
-    #true_graph = nx.Graph()
-    #true_graph.add_edges_from(edges)
+    true_graph = nx.Graph()
+    true_graph.add_edges_from(edges)
 
     # use BallTree for efficient graph construction
     print "construct BallTree"
-    vertices = sorted(embeddings.keys())
+    vertices = list(true_graph.nodes())
     embeddings_array = np.array([embeddings[v] for v in vertices])
     bt = BallTree(embeddings_array, metric=distance)
 
-    predicted_edges = set()
-    print "predict edges"
-    for v in embeddings.keys():
-        coords = embeddings[v]
-        neigh_idx = bt.query_radius(coords, R)
-        neigh = [vertices[i] for i in neigh_idx[0].tolist() if vertices[i] != v]
-        predicted_edges.update([make_edge(v, ne) for ne in neigh])
-    report.append(['total_predicted_edges', len(predicted_edges)])
+    if False:
+        # depends on R, bad for subgraphs -- not used
+        n = len(vertices)
+        R = 2 * np.log(n)
+        coshR = np.cosh(R)
 
-    # contingency matrix
-    print "compute contingency matrix"
-    report.append(['true positive', len(edges & predicted_edges)])
-    report.append(['false positive', len(predicted_edges - edges)])
-    report.append(['false negative', len(edges - predicted_edges)])
-    report.append(['true negative', n*(n-1)/2 - len(edges | predicted_edges)])
+        predicted_edges = set()
+        print "predict edges"
+        for v in vertices:
+            coords = embeddings[v]
+            neigh_idx = bt.query_radius(coords, R)
+            neigh = [vertices[i] for i in neigh_idx[0].tolist() if vertices[i] != v]
+            predicted_edges.update([make_edge(v, ne) for ne in neigh])
+        report.append(['total_predicted_edges', len(predicted_edges)])
+
+        # contingency matrix
+        print "compute contingency matrix"
+        report.append(['true positive', len(edges & predicted_edges)])
+        report.append(['false positive', len(predicted_edges - edges)])
+        report.append(['false negative', len(edges - predicted_edges)])
+        report.append(['true negative', n*(n-1)/2 - len(edges | predicted_edges)])
 
     degrees = defaultdict(int)
     print "compute number of correct directed arcs"
