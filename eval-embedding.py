d2cdd0b55ec6d4a00168ef996995e267f7d1e774
horoshenkih <horoshenkih91@gmail.com>
2016-10-15 18:13:39 +0300

    evaluate greedy routing

--- eval-embedding.py
+++ eval-embedding.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 from collections import defaultdict
+from itertools import combinations
 
 import numpy as np
 import networkx as nx
@@ -8,18 +9,96 @@ from sklearn.neighbors import BallTree
 
 from lib.graph import read_graph_from_file, read_embeddings_from_file, distance, make_edge
 
-def evaluate_embeddings(embeddings, edges):
+def evaluate_embeddings(embeddings, edges, cda=True, greedy_routing=False):
     "evaluate quality of embeddings compared with real elge set"
     report = []
 
+    # get connected component
     true_graph = nx.Graph()
     true_graph.add_edges_from(edges)
+    Gcc=sorted(nx.connected_component_subgraphs(true_graph), key=len, reverse=True)
+    true_graph=Gcc[0]
 
     # use BallTree for efficient graph construction
     print "construct BallTree"
     vertices = list(true_graph.nodes())
+    n = len(vertices)
     embeddings_array = np.array([embeddings[v] for v in vertices])
     bt = BallTree(embeddings_array, metric=distance)
+
+    degrees = defaultdict(int)
+    print "compute number of correct directed arcs"
+    for v1, v2 in edges:
+        degrees[v1] += 1
+        degrees[v2] += 1
+
+    # compute number of correct DIRECTED arcs assuming that degrees are known
+    if cda:
+        all_correct_arcs = set()
+        for v in vertices:
+            degree = degrees[v]
+            dist, ind = bt.query(embeddings[v], k=degree+1)  # one of neighbors is vertex inself
+            neigh = [vertices[i] for i in ind[0].tolist() if vertices[i] != v]
+            for ne in neigh:
+                if make_edge(v, ne) in edges:
+                    all_correct_arcs.add((v, ne))
+        report.append(['ratio of correct arcs for known degrees', float(len(all_correct_arcs)) / (2 * len(edges))])
+
+    if greedy_routing:
+        print "compute greedy routing efficiency"
+        max_pairs = 10000
+        random_pairs = set()
+        if n * (n-1) / 2 <= max_pairs:
+            random_pairs = set(combinations(vertices, 2))
+        else:
+            while(len(random_pairs) < max_pairs):
+                v1 = np.random.choice(vertices)
+                v2 = np.random.choice(vertices)
+                if v1 != v2:
+                    random_pairs.add((v1, v2))
+
+        total_distribution = defaultdict(int)
+        success_distribution = defaultdict(int)
+        for i, pair in enumerate(random_pairs):
+            src, dst = pair
+            # best path
+            best_path_length = nx.shortest_path_length(true_graph, source=src, target=dst)
+            total_distribution[0] += 1
+            total_distribution[best_path_length] += 1
+
+            # greedy path
+            curr_src = src
+            path_length = 0
+            seen = set()
+            while curr_src != dst:
+                seen.add(curr_src)
+                # find neighbor closest to destination
+                unseen_neighbors = filter(lambda x: x not in seen, true_graph.neighbors(curr_src))
+                if not len(unseen_neighbors):
+                    # greedy algorithm stuck in 'leaf'
+                    path_length = np.nan
+                    break
+
+                def curr_distance(v):
+                    return distance(embeddings[dst], embeddings[v])
+                closest_neigh = min(unseen_neighbors, key=curr_distance)
+                path_length += 1
+                curr_src = closest_neigh
+
+            if path_length == best_path_length:
+                success_distribution[0] += 1
+                success_distribution[best_path_length] += 1
+        all_success = success_distribution[0]
+        all_total = total_distribution[0]
+        all_ratio = float(all_success) / all_total * 100
+        print "Total: {} / {} ({:.2f} %)".format(all_success, all_total, all_ratio)
+        for pl in sorted(set(total_distribution.keys()) | set(success_distribution.keys())):
+            if pl == 0:
+                continue
+            total = total_distribution.get(pl, 0)
+            success = success_distribution.get(pl, 0)
+            ratio = float(success) / total * 100
+            print "Path length = {}: {} / {} ({:.2f} %)".format(pl, success, total, ratio)
 
     if False:
         # depends on R, bad for subgraphs -- not used
@@ -43,59 +122,25 @@ def evaluate_embeddings(embeddings, edges):
         report.append(['false negative', len(edges - predicted_edges)])
         report.append(['true negative', n*(n-1)/2 - len(edges | predicted_edges)])
 
-    degrees = defaultdict(int)
-    print "compute number of correct directed arcs"
-    for v1, v2 in edges:
-        degrees[v1] += 1
-        degrees[v2] += 1
-
-    # compute number of correct DIRECTED arcs assuming that degrees are known
-    # TODO the same for non-edges
-    all_correct_arcs = set()
-    for v in vertices:
-        degree = degrees[v]
-        dist, ind = bt.query(embeddings[v], k=degree+1)  # one of neighbors is vertex inself
-        neigh = [vertices[i] for i in ind[0].tolist() if vertices[i] != v]
-        for ne in neigh:
-            if make_edge(v, ne) in edges:
-                all_correct_arcs.add((v, ne))
-    report.append(['ratio of correct arcs for known degrees', float(len(all_correct_arcs)) / (2 * len(edges))])
-
     return report
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('graph_file')
     parser.add_argument('embeddings_file')
-    parser.add_argument('-p', '--plot', action='store_true', help='plot visualization')
+    parser.add_argument('--cda', action='store_true', help='correct directed arcs')
+    parser.add_argument('--gr', action='store_true', help='evaluate greedy routing')
     args = parser.parse_args()
 
     vertices, edges = read_graph_from_file(args.graph_file)
     embeddings = read_embeddings_from_file(args.embeddings_file)
 
     print "Evaluate embeddings"
-    report = evaluate_embeddings(embeddings, edges)
+    report = evaluate_embeddings(embeddings, edges, cda=args.cda, greedy_routing=args.gr)
 
     print 'report'
     for name, value in report:
         print '{}: {}'.format(name, value)
-
-    if args.plot:
-        import matplotlib.pyplot as plt
-        # vertices
-        r, phi = zip(*embeddings.values())
-        ax = plt.subplot(111, projection='polar')
-        ax.plot(phi, r, marker='o', linewidth=0)
-
-        # edges
-        # TODO
-        '''
-        for (v1, v2) in graph.edges():
-            r1, phi1 = v1
-            r2, phi2 = v2
-            ax.plot((phi1, phi2), (r1, r2), color='g')
-        '''
-        plt.show()
 
 if __name__ == '__main__':
     main()
